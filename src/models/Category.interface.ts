821c64b5081b13e1f507315201c2dc3202b540a0
HenrySilverio <silveriohenriqueb@gmail.com>
2025-05-29 08:43:54 -0300

    Add Category interface and model definitions

---
+++ src/models/Category.interface.ts
@@ -0,0 +1,10 @@
+export interface ICategory extends Document {
+  name: string;
+  description?: string;
+  color?: string;
+  icon?: string;
+  isSavings: boolean;
+  isExpense: boolean;
+  createAt: Date;
+  upDateAt: Date;
+}
---
+++ src/models/Category.models.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { ICategory } from './Category.interface';
+
+const CategorySchema: Schema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, trim: true },
+    color: { type: String, default: '#4285f4' },
+    icon: { type: String },
+    isSavings: { type: Boolean, default: false },
+    isExpense: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.models.Category ||
+  mongoose.model<ICategory>('Category', CategorySchema);
